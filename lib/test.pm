a20fb19a2486f47a12d385fafead415456611014
yukiyan42 <yukiyan42@gmail.com>
2016-05-25 00:18:14 +0900

    initial import

---
+++ lib/test.pm
@@ -0,0 +1,47 @@
+package test;
+use strict;
+use warnings;
+use utf8;
+our $VERSION='0.01';
+use 5.008001;
+use test::DB::Schema;
+use test::DB;
+
+use parent qw/Amon2/;
+# Enable project local mode.
+__PACKAGE__->make_local_context();
+
+my $schema = test::DB::Schema->instance;
+
+sub db {
+    my $c = shift;
+    if (!exists $c->{db}) {
+        my $conf = $c->config->{DBI}
+            or die "Missing configuration about DBI";
+        $c->{db} = test::DB->new(
+            schema       => $schema,
+            connect_info => [@$conf],
+            # I suggest to enable following lines if you are using mysql.
+            # on_connect_do => [
+            #     'SET SESSION sql_mode=STRICT_TRANS_TABLES;',
+            # ],
+        );
+    }
+    $c->{db};
+}
+
+1;
+__END__
+
+=head1 NAME
+
+test - test
+
+=head1 DESCRIPTION
+
+This is a main context class for test
+
+=head1 AUTHOR
+
+test authors.
+
---
+++ t/00_compile.t
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+use test;
+use test::Web;
+use test::Web::View;
+use test::Web::ViewFunctions;
+
+use test::DB::Schema;
+use test::Web::Dispatcher;
+
+
+pass "All modules can load.";
+
+done_testing;
